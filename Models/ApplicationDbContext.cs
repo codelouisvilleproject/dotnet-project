c6f3b01d078dcbb154d8172759a954d582951022
Nesto <bumbolio@gmail.com>
2017-12-25 14:17:58 -0500

    Added a default database context, needs to be modified to connect to postgress.  For now, it uses an in-memory database.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_project.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
