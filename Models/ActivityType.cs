8af69a20fc9570c0b69cdade9a0ac1cc9cecc5c0
Christopher Coe <christopherdcoe@gmail.com>
2018-01-29 19:58:37 -0500

    Created class file with comments of what's needed

---
+++ Models/ActivityType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_project.Models
+{
+    public class ActivityType
+    {
+       
+
+
+       /*
+        CREATE TABLE activitytypes (
+            id UUID PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            description VARCHAR NOT NULL
+        );
+        */
+    }
+}
