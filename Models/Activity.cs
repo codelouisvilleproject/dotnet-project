9fe88c9fa8e68817d8698be87aaafff02460aa95
Andrew Witten <andrewjwitten@gmail.com>
2018-01-31 23:34:16 -0500

    Created Activity class to match activity from activities table.

---
+++ Models/Activity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetproject.Models
+{
+    public class Activity
+    {
+        public Activity()
+        {
+        }
+
+        public Activity(Guid id, Guid userid, Guid activitytypeid, int durationmilliseconds)
+        {
+            id = Id;
+            userid = UserId;
+            activitytypeid = ActivityTypeId;
+            durationmilliseconds = DurationMilliseconds;
+        }
+
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ActivityTypeId { get; set; }
+        public int DurationMilliseconds { get; set; }
+    }
+}
