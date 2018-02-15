76b2abfb2ac919d75e5d26802a3a166218ac8b8a
awitten <andrewjwitten@gmail.com>
2018-02-15 08:41:09 -0500

    Added ActivityTypeController and DbSet.

---
+++ Controllers/ActivityTypeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_project.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Activities")]
+    public class ActivityTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Activities
+        [HttpGet]
+        public IEnumerable<ActivityType> GetActivityType()
+        {
+
+            return _context.ActivityTypes;
+
+        }
+
+        // GET: api/Activities/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivityType([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var activity = await _context.ActivityTypes.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(activity);
+        }
+    }
